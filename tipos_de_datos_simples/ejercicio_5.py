d6b1f7805f637dc4e7812b97fa430bc747588d52
ruben1909 <rubenfm101909@gmail.com>
2026-09-24 14:18:20 +0200

    ejercicio_5

---
+++ tipos_de_datos_simples/ejercicio_5.py
@@ -0,0 +1,5 @@
+num_horas = int(input("introduce el numero de horas trabajadas: "))
+coste_hora = int(input("introduce el coste por hora(€): "))
+pago = num_horas*coste_hora
+
+print(f"el pago es de {pago} €")
